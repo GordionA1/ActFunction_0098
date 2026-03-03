2b9bdb4c979c3a45d769f74381a313a328c5255e
GordionA1 <farisazka2506@gmail.com>
2026-03-03 16:44:50 +0700

    mengimplementasikan prosedure dan fungsi

--- ParameterLuas.cpp
+++ ParameterLuas.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 //Deklarasi Global
     float p,l;
+
+//implementasi prosedure & fungsi
+void input(){
+    cout << "masukkan panjang :";
+    cin >> p;
+    cout << "masukkan lebar :";
+    cin >> l;
+}
